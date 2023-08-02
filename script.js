8f8d248c9ff017b7d74312da0a7288ed0b65fe58
ahmet360 <93016308+ahmet360@users.noreply.github.com>
2023-08-02 14:48:22 -0400

    u

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
-// ... (existing code) ...
+// Fact of the Day data (replace with your facts)
+const factsOfTheDay = [
+    "Fact 1: AI-powered VR is revolutionizing the way we experience virtual worlds.",
+    "Fact 2: Virtual Reality has endless applications, from gaming to training simulations.",
+    "Fact 3: Augmented Reality enhances our real-world experiences through digital overlays.",
+    "Fact 4: Mixed Reality combines virtual and physical worlds for interactive experiences.",
+    "Fact 5: Comunnitech's AI algorithms create breathtaking VR worlds from simple prompts."
+];
 
 // Function to get a random fact
 function getRandomFact() {
-    const facts = [
-        "AI is revolutionizing the VR world!",
-        "VR and AR will reshape how we interact with technology.",
-        "MR combines virtual and real-world experiences seamlessly.",
-        "AI can generate stunning VR environments with just a prompt.",
-        // Add more facts here
-    ];
-    return facts[Math.floor(Math.random() * facts.length)];
+    return factsOfTheDay[Math.floor(Math.random() * factsOfTheDay.length)];
 }
 
-// Function to update the fact of the day
-function updateFactOfTheDay() {
-    const factElement = document.getElementById('fact');
+// Function to display the fact of the day
+function displayFactOfTheDay() {
+    const factElement = document.getElementById("fact");
     factElement.textContent = getRandomFact();
 }
 
-// Event listener for the hidden button to change the fact
-const changeFactBtn = document.getElementById('changeFactBtn');
-changeFactBtn.addEventListener('click', () => {
-    updateFactOfTheDay();
+// Function to reset fact of the day after 24 hours
+function resetFactOfTheDay() {
+    setInterval(() => {
+        displayFactOfTheDay();
+    }, 86400000); // 24 hours in milliseconds
+}
+
+// Function to handle the click event on the hidden button
+document.getElementById("changeFactBtn").addEventListener("click", () => {
+    displayFactOfTheDay();
 });
 
-// Initialize the fact of the day
-updateFactOfTheDay();
+// Call the function to display the initial fact of the day
+displayFactOfTheDay();
 
-// ... (existing code) ...
+// Call the function to reset fact of the day every 24 hours
+resetFactOfTheDay();
